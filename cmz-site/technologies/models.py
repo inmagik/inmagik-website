6422cc74d8c9b05e64ac162c86a6f5a03a57dfa5
Mauro Bianchi <bianchimro@gmail.com>
2016-09-30 13:29:59 +0200

    moved storage to s3

--- cmz-site/technologies/models.py
+++ cmz-site/technologies/models.py
@@ -5,10 +5,14 @@ from django.conf import settings
 from hvad.models import TranslatableModel, TranslatedFields
 from hvad.utils import get_translation_aware_manager
 
+#from django_s3_storage.storage imporS3Storage
+
 # Create your models here.
 class Tech(TranslatableModel):
 
-    icon = models.ImageField(null=True, blank=True, upload_to="technology-icons")
+    icon = models.ImageField(null=True, blank=True, upload_to="technology-icons"
+        #, storage=S3Storage
+    )
     translations = TranslatedFields(
         title = models.CharField(max_length=255),
         description = models.TextField(null=True, blank=True)
--- cmz-site/website/settings.py
+++ cmz-site/website/settings.py
@@ -29,3 +29,12 @@ COOKIECONSENT_OPTIONS = {
     "dismiss" : "OK!",
     "learnMore" : "More Info"
 }
+
+try:
+    from secrets import *
+    AWS_S3_BUCKET_NAME = "inmagikweb"
+    AWS_REGION = "eu-central-1"
+    AWS_S3_GZIP = True
+    DEFAULT_FILE_STORAGE = "django_s3_storage.storage.S3Storage".
+except:
+    raise
