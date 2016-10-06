7aacc280ca5fe6c40eef52900ddb741ee4b320d4
Mauro Bianchi <bianchimro@gmail.com>
2016-10-06 22:30:54 +0200

    empowered cmz_files with thumbnail for images in admin. wow.

--- cmz-site/cmz_files/admin.py
+++ cmz-site/cmz_files/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import NamedFile
 
-admin.site.register(NamedFile)
+class NamedFileAdmin(admin.ModelAdmin):
+    readonly_fields = ['thumb']
+    list_display = ["name", "thumb_preview"]
+
+admin.site.register(NamedFile, NamedFileAdmin)
--- cmz-site/cmz_files/models.py
+++ cmz-site/cmz_files/models.py
@@ -3,13 +3,51 @@ from __future__ import unicode_literals
 from django.db import models
 from django.conf import settings
 from django_s3_storage.storage import S3Storage
+from django.utils.html import mark_safe
+
+from PIL import Image
+import StringIO
+from django.core.files.uploadedfile import InMemoryUploadedFile
 
 # Create your models here.
 class NamedFile(models.Model):
     name = models.CharField(max_length=300)
-    upload = models.ImageField(null=True, blank=True,
+    upload = models.FileField(null=True, blank=True,
         upload_to="cmz-files",
         storage=S3Storage())
+
+    thumb = models.FileField(
+        null=True, blank=True, editable=False,
+        upload_to="cmz-files",
+        storage=S3Storage()
+    )
+
+    def thumb_preview(self):
+        if self.thumb:
+            return mark_safe('<img src="%s" width="100" height="auto" />' % (self.thumb.url))
+        return ""
+
+    thumb_preview.short_description = 'Thumb'
+
+
+    def save(self, *args, **kwargs):
+        self.thumb.delete(False)
+        try:
+            im = Image.open(self.upload.file)
+            size = (100, 100*im.size[0]/im.size[1])
+            im.thumbnail(size, Image.ANTIALIAS)
+            thumb_io = StringIO.StringIO()
+            im.save(thumb_io, format='JPEG')
+            # Create a new Django file-like object to be used in models as ImageField using
+            # InMemoryUploadedFile.  If you look at the source in Django, a
+            # SimpleUploadedFile is essentially instantiated similarly to what is shown here
+            thumb_file = InMemoryUploadedFile(thumb_io, None, '%s.thumb.jpg' % self.upload.name,
+                'image/jpeg', thumb_io.len, None)
+            self.thumb = thumb_file
+        except:
+            pass
+
+        return super(NamedFile, self).save(*args, **kwargs)
 
     def __unicode__(self):
         return self.name
@@ -24,3 +62,4 @@ from django.dispatch.dispatcher import receiver
 def mymodel_delete(sender, instance, **kwargs):
     # Pass false so FileField doesn't save the model.
     instance.upload.delete(False)
+    instance.thumb.delete(False)
