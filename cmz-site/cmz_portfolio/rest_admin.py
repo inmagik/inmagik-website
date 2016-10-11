f23b2a98970eef9182347be1f84f0d130549f1cb
Mauro Bianchi <bianchimro@gmail.com>
2016-10-11 17:38:59 +0200

    multiparser for portfolio

--- cmz-site/cmz_portfolio/rest_admin.py
+++ cmz-site/cmz_portfolio/rest_admin.py
@@ -3,8 +3,10 @@ from django_rest_admin.register import rest_admin, RestAdminConfig
 from hvad.contrib.restframework import TranslatableModelSerializer
 from rest_framework.viewsets import ModelViewSet
 from django.utils import translation
+from rest_framework.parsers import FileUploadParser, MultiPartParser, JSONParser
 
 class PortfolioItemViewSet(ModelViewSet):
+    parser_classes = ( MultiPartParser, JSONParser )
     def get_queryset(self):
         return PortfolioItem.objects.language(translation.get_language()).all()
 
