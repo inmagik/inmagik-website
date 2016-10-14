5c6f5de499ee76a09a69bec1152c2a13fd5448ea
Mauro Bianchi <bianchimro@gmail.com>
2016-10-14 02:40:48 +0200

    aws cache

--- cmz-site/website/settings.py
+++ cmz-site/website/settings.py
@@ -41,6 +41,7 @@ try:
     AWS_S3_BUCKET_NAME = "inmagikweb"
     AWS_REGION = "eu-central-1"
     AWS_S3_GZIP = True
+    AWS_S3_MAX_AGE_SECONDS = 60*60*24*10  # 10 days.
     #DEFAULT_FILE_STORAGE = "django_s3_storage.storage.S3Storage".
 except:
     raise
